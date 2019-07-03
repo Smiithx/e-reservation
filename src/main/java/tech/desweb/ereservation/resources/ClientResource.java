df37a1a7a7cfebd3fa425cfbc72fa3d975d5c4ae
Andres Gonzalez <smiith17@gmail.com>
2019-07-02 22:09:36 -0500

    adding clientResource

---
+++ src/main/java/tech/desweb/ereservation/resources/ClientResource.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package tech.desweb.ereservation.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class that represent the web service client
+ * @author smiit
+ *
+ */
+@RestController
+@RequestMapping("/api/client")
+public class ClientResource {
+
+}
