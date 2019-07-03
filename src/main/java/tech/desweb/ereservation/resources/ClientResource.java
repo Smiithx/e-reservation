69400196d5c0ac1052207cc930c4d5ae24326b99
Andres Gonzalez <smiith17@gmail.com>
2019-07-02 23:07:35 -0500

    Documentation of REST Services with Swagger

--- src/main/java/tech/desweb/ereservation/resources/ClientResource.java
+++ src/main/java/tech/desweb/ereservation/resources/ClientResource.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import tech.desweb.ereservation.model.Client;
 import tech.desweb.ereservation.resources.vo.ClientVO;
 import tech.desweb.ereservation.services.ClientService;
@@ -26,6 +31,7 @@ import tech.desweb.ereservation.services.ClientService;
  */
 @RestController
 @RequestMapping("/api/client")
+@Api(tags = "client")
 public class ClientResource {
 	private final ClientService clientService;
 	
@@ -34,6 +40,11 @@ public class ClientResource {
 	}
 	
 	@PostMapping
+	@ApiOperation(value = "Create Client",notes = "Service for create new client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201,message = "The client has been created successfully!"),
+			@ApiResponse(code = 400,message = "Invalid request")
+	})
 	public ResponseEntity<Client> createClient(@RequestBody ClientVO clientVO){
 		Client client = new Client();
 		client.setFirstName(clientVO.getFirstName());
@@ -46,6 +57,11 @@ public class ClientResource {
 	}
 	
 	@PutMapping("/{identification}")
+	@ApiOperation(value = "Update Client",notes = "Service for update client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200,message = "The client has been updated successfully!"),
+			@ApiResponse(code = 404,message = "The client has not been found")
+	})
 	public ResponseEntity<Client> updateClient(@PathVariable("identification") String identification,ClientVO clientVO){
 		Client client = this.clientService.findByIdentification(identification);
 		if(client == null) {
@@ -62,6 +78,11 @@ public class ClientResource {
 	}
 	
 	@DeleteMapping("/{identification}")
+	@ApiOperation(value = "Delete Client",notes = "Service for delete client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200,message = "The client has been deleted successfully!"),
+			@ApiResponse(code = 404,message = "The client has not been found")
+	})
 	public void deleteClient(@PathVariable("identification") String identification) {
 		Client client = this.clientService.findByIdentification(identification);
 		if(client != null) {
@@ -69,6 +90,12 @@ public class ClientResource {
 		}
 	}
 	
+	@GetMapping
+	@ApiOperation(value = "List Client",notes = "Service for list client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200,message = "The clients has been found!"),
+			@ApiResponse(code = 404,message = "The clients has not been found")
+	})
 	public ResponseEntity<List<Client>> findAll(){
 		return ResponseEntity.ok(this.clientService.findAll());
 	}
